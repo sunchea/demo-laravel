fb5255a148c35a561b8636979d1bbdd9c432507c
a.zhamkov <a.zhamkov@vpene.ru>
2021-06-07 22:44:50 +0300

    First commit

---
+++ resources/views/page.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Example {{ $item }} <span class="badge bg-secondary">{{ $label }}</span></h1>
+    <h2>Example {{ $item }} <span class="badge bg-secondary">{{ $label }}</span></h2>
+    <h3>Example {{ $item }} <span class="badge bg-secondary">{{ $label }}</span></h3>
+    <h4>Example {{ $item }} <span class="badge bg-secondary">{{ $label }}</span></h4>
+    <h5>Example {{ $item }} <span class="badge bg-secondary">{{ $label }}</span></h5>
+    <h6>Example {{ $item }} <span class="badge bg-secondary">{{ $label }}</span></h6>
+@endsection
